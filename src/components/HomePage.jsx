0e82a263222082e38ea1d30d9de1b60d198a34c8
shivang100 <91174389+shivang100@users.noreply.github.com>
2025-04-01 17:20:02 +0530

    logochange

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -10,19 +10,19 @@ export default function HomePage() {
       <div className="relative lg:flex md:flex lg:items-center lg:justify-center md:justify-center md:items-center justify-center items-center h-full">
         <div className="flex flex-col lg:justify-center md:justify-center md:items-center justify-center items-center py-60">
           <Image
-            src="/bglogo.png"
+            src="/logo1.png"
             height={5000}
             width={7000}
             alt="logo"
-            className="lg:h-[200px] lg:w-[300px] md:h-[100px] md:w-[150px] h-[150px] w-[175px]"
+            className="lg:h-[450px] lg:w-[450px] md:h-[100px] md:w-[150px] h-[150px] w-[175px]"
           />
-          <Image
+          {/* <Image
             src="/title2.png"
             height={5000}
             width={7000}
             alt="logo"
             className="lg:h-[200px] lg:w-[300px] md:h-[100px] md:w-[150px] h-[150px] w-[175px] "
-          />
+          /> */}
         </div>
       </div>
     </div>
